8ac18d6ee7caea045e7255852273bff66cdb0423
Richard Wattenbarger <github@wattenbarger.net>
2021-10-11 20:37:56 -0400

    added type hints

--- pysimpleguitemplate.py
+++ pysimpleguitemplate.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 
 import PySimpleGUI as sg
+from collections.abc import Iterable
+from typing import Tuple
 
-DEFAULT_THEME = "SystemDefault"
+DEFAULT_THEME:str = "DarkGreen"
 
-def set_theme(theme_name=DEFAULT_THEME):
+def set_theme(theme_name:str=DEFAULT_THEME) -> None:
     sg.theme(theme_name)
 
-def set_font(font_descr=("Arial", 14)):
+def set_font(font_descr:Tuple[str, int]=("Arial", 14)) -> None:
     sg.set_options(font=font_descr)
     
-def make_layout():
+def make_layout() -> Iterable[Iterable[sg.Element]]:
     return [ [ sg.Text("Name"),
                sg.Input(key="-NAME-")] ]
 
-def make_window(title, layout):
-    return sg.Window(title, layout)
+def make_window(title: str, layout: Iterable[Iterable[sg.Element]]) \
+        -> sg.Window:
+    return sg.Window(title, layout, finalize=True)
 
-def main():
+def main() -> None:
     set_theme()
     set_font()
-    layout = make_layout()
-    window = make_window("Window Title", layout)
+    layout: Iterable[Iterable[sg.Element]]= make_layout()
+    window: sg.Window = make_window("Window Title", layout)
 
     while True:
         event, values = window.read()
-        if event == WIN_CLOSED:
+        if event == sg.WIN_CLOSED:
             break
         else:
             print(f"Event:  {event}")
